af7dbac995036bd776405928ef33b974b4be26d0
Duale Siad <me@wale.id.au>
2022-11-15 21:51:09 +1100

    fix: mobile responsive view

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import remarkUnwrapImages from "remark-unwrap-images";
+import remarkTwemoji from "remark-twemoji";
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wale.au',
 	integrations: [mdx(), sitemap()],
 	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkTwemoji],
 		syntaxHighlight: "shiki",
 		shikiConfig: {
 			theme: 'rose-pine',
