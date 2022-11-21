6f23484d05a8c251f5dbfac3d56414944aa533f6
Duale Siad <me@wale.id.au>
2022-11-21 13:09:41 +1100

    add rehype plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,16 @@ import sitemap from '@astrojs/sitemap';
 import remarkUnwrapImages from "remark-unwrap-images";
 import remarkTwemoji from "remark-twemoji";
 
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wale.au',
 	integrations: [mdx(), sitemap()],
 	markdown: {
+		extendDefaultPlugins: true,
 		remarkPlugins: [remarkUnwrapImages, remarkTwemoji],
+		rehypePlugins: [rehypeAutolinkHeadings],
 		syntaxHighlight: "shiki",
 		shikiConfig: {
 			theme: 'rose-pine',
