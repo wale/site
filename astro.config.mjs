2b211911733441494980c77b7d142bc9fcb22394
Duale Siad <me@wale.id.au>
2023-02-16 16:21:21 +1100

    Rewrite home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 import remarkUnwrapImages from "remark-unwrap-images";
 import remarkTwemoji from "remark-twemoji";
-
-import rehypeSlug from "rehype-slug"
+import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
-// https://astro.build/config
+import mochaTheme from "./src/catppuccin/mocha-theme.json";
+
 export default defineConfig({
-	site: 'https://wale.au',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		extendDefaultPlugins: true,
-		remarkPlugins: [remarkUnwrapImages, remarkTwemoji],
-		rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
-		syntaxHighlight: "shiki",
-		shikiConfig: {
-			theme: 'rose-pine',
-			wrap: true
-		},
-	}
+  site: 'https://wale.au',
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkUnwrapImages, remarkTwemoji],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: mochaTheme,
+      wrap: true
+    }
+  }
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans]
+			}
+		},
+	},
+	plugins: [
+		require("@catppuccin/tailwindcss")({
+			prefix: false,
+			defaultFlavour: "mocha",
+		})
+	],
+}
